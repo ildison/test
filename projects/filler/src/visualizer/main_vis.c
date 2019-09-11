1a1834bedbf2aa2d261eafece081c160f92589ca
ildison <ildarusmanov92@gmail.com>
2019-09-11 18:06:34 +0300

    complite parsing board

--- projects/filler/src/visualizer/main_vis.c
+++ projects/filler/src/visualizer/main_vis.c
@@ -6,7 +6,7 @@
 /*   By: cormund <cormund@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 15:16:22 by cormund           #+#    #+#             */
-/*   Updated: 2019/09/10 12:30:27 by cormund          ###   ########.fr       */
+/*   Updated: 2019/09/11 16:17:14 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,20 @@ void			error()
 
 void			visualizer()
 {
+	t_step		*step;
 	t_game		*game;
 	char		b;
 
+	step = NULL;
 	game = (t_game*)malloc(sizeof(t_game));
+	game->start_step = &step;
 	game->p1 = NULL;
 	game->p1_tokens = 0;
 	game->p2_tokens = 0;
-	if (read(STD_OUT, &b, 1)) //? while
+	while (read(STD_OUT, &b, 1)) //? while
 		read_board(game);
+	int fd = open("test", O_WRONLY);
+	ft_printf("%vcormund %d\nsmorty %d", fd, game->p1_tokens, game->p2_tokens);
 }
 
 int				main()
