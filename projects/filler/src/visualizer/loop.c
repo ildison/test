ebb2da25fb64c1056a1b962589b6dfefed0bba45
ildison <ildarusmanov92@gmail.com>
2019-09-13 15:58:01 +0300

    working version

--- projects/filler/src/visualizer/loop.c
+++ projects/filler/src/visualizer/loop.c
@@ -6,7 +6,7 @@
 /*   By: cormund <cormund@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/12 18:28:31 by cormund           #+#    #+#             */
-/*   Updated: 2019/09/13 11:54:12 by cormund          ###   ########.fr       */
+/*   Updated: 2019/09/13 14:56:34 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void		events(t_vis *vis)
 {
 	if (vis->e.type == SDL_QUIT || vis->keyState[SDL_SCANCODE_ESCAPE])
 		vis->quit = SDL_TRUE;
-	else if (vis->e.type == SDL_KEYDOWN && vis->keyState[SDL_SCANCODE_ESCAPE])
+	else if (vis->e.type == SDL_KEYDOWN && vis->keyState[SDL_SCANCODE_ESCAPE] && vis->pause == SDL_FALSE)
 		vis->pause = SDL_TRUE;
-	else if (vis->e.type == SDL_KEYUP && vis->keyState[SDL_SCANCODE_ESCAPE])
+	else if (vis->e.type == SDL_KEYDOWN && vis->keyState[SDL_SCANCODE_ESCAPE] && vis->pause == SDL_TRUE)
 		vis->pause = SDL_FALSE;
 }
 
@@ -29,11 +29,11 @@ void		render_update(t_game *game, t_vis *vis, t_step *step)
 	SDL_RenderClear(vis->ren);
 	if (step)
 	{
-		SDL_SetRenderDrawColor(vis->ren, 89, 89, 89, SDL_ALPHA_OPAQUE);
+		SDL_SetRenderDrawColor(vis->ren, 27, 36, 55, SDL_ALPHA_OPAQUE);
 		SDL_RenderFillRect(vis->ren, &vis->bgrnd_board);
-		SDL_SetRenderDrawColor(vis->ren, 255, 191, 142, SDL_ALPHA_OPAQUE);
+		SDL_SetRenderDrawColor(vis->ren, 254, 110, 125, SDL_ALPHA_OPAQUE);
 		SDL_RenderFillRects(vis->ren, step->p1, step->n_p1);
-		SDL_SetRenderDrawColor(vis->ren, 133, 56, 214, SDL_ALPHA_OPAQUE);
+		SDL_SetRenderDrawColor(vis->ren, 199, 89, 156, SDL_ALPHA_OPAQUE);
 		SDL_RenderFillRects(vis->ren, step->p2, step->n_p2);
 		SDL_SetRenderDrawColor(vis->ren, 216, 216, 216, SDL_ALPHA_OPAQUE);
 		SDL_RenderFillRects(vis->ren, step->piece, step->n_pc);
@@ -53,12 +53,10 @@ void		loop(t_game *game, t_vis *vis, t_step *step)
 		if (step && !step->fin && !vis->pause)
 			step = step->next;
 		if (!vis->pause && !step && read(STD_OUT, &b, 1))
-		{
 			step = read_board(game);
-		}
 		while (SDL_PollEvent(&vis->e))
 			events(vis);
 		render_update(game, vis, step);
-		SDL_Delay(1000);
+		// SDL_Delay(1000);
 	}
 }
