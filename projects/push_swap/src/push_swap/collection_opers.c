da04a86cb628c00d689d306495328dbeb1dafdf2
Chugtai Ormund <cormund@mi-t4.21-school.ru>
2019-10-04 19:15:25 +0300

    begin coding sort_first_hundred

--- projects/push_swap/src/push_swap/collection_opers.c
+++ projects/push_swap/src/push_swap/collection_opers.c
@@ -6,13 +6,13 @@
 /*   By: cormund <cormund@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/04 12:29:49 by cormund           #+#    #+#             */
-/*   Updated: 2019/10/04 12:54:44 by cormund          ###   ########.fr       */
+/*   Updated: 2019/10/04 16:55:46 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	push_oper(t_oper **op, char *oper)
+static void	add_oper(t_oper **op, char *oper)
 {
 	t_oper	*tmp;
 
@@ -37,24 +37,24 @@ static void	push_oper(t_oper **op, char *oper)
 
 void		ps_swap(t_ps *ps, t_stack **top)
 {
-	*top == ps->a ? push_oper(&ps->opers, "sa") : push_oper(&ps->opers, "sb");
-	swap(top);
+	if (swap(top))
+		*top == ps->a ? add_oper(&ps->opers, "sa") : add_oper(&ps->opers, "sb");
 }
 
 void		ps_push(t_ps *ps, t_stack **dst, t_stack **src)
 {
-	*dst == ps->a ? push_oper(&ps->opers, "pa") : push_oper(&ps->opers, "pb");
-	push(dst, src);
+	if (push(dst, src))
+		*dst == ps->a ? add_oper(&ps->opers, "pa") : add_oper(&ps->opers, "pb");
 }
 
 void		ps_rotate(t_ps *ps, t_stack **top)
 {
-	*top == ps->a ? push_oper(&ps->opers, "ra") : push_oper(&ps->opers, "rb");
-	rotate(top);
+	if (rotate(top))
+		*top == ps->a ? add_oper(&ps->opers, "ra") : add_oper(&ps->opers, "rb");
 }
 
 void		ps_reverse(t_ps *ps, t_stack **top)
 {
-	*top == ps->a ? push_oper(&ps->opers, "rra") : push_oper(&ps->opers, "rrb");
-	reverse(top);
+	if (reverse(top))
+		*top == ps->a ? add_oper(&ps->opers, "rra") : add_oper(&ps->opers, "rrb");
 }
