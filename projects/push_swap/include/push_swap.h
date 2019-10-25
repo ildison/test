145b84a7e427bcd748ca4dd777dd980216059b3b
ildison <ildarusmanov92@gmail.com>
2019-10-25 15:22:41 +0300

    before checkout

--- projects/push_swap/include/push_swap.h
+++ projects/push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cormund <cormund@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 15:54:39 by cormund           #+#    #+#             */
-/*   Updated: 2019/10/06 20:59:39 by cormund          ###   ########.fr       */
+/*   Updated: 2019/10/25 10:31:18 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 
 # include "ps_shared.h"
 
-# define PS_FIRST_INDEX 1
-# define PS_MAX_INDEX_IN_STACK_B ps->sz_a - 3
-# define PS_PREVIOUS_MAX PS_MAX_INDEX_IN_STACK_B - 1
-# define PS_COUNT_MIN_AT_BOTTOM 0
-# define SIZE_A ps->sz_a
-# define SIZE_B ps->sz_b
-# define TOP_A (*a)->i
-# define SECOND_A (*a)->next->i
-# define TOP_B (*b)->i
-# define SECOND_B (*b)->next->i
-# define IS_OPER_OF_STACK_A (op->next->oper[1] == 'a' || op->next->oper[2] == 'a')
-# define IS_OPER_OF_STACK_B (op->next->oper[1] == 'b' || op->next->oper[2] == 'b')
+# define PS_FIRST_INDEX				1
+# define PS_MAX_INDEX_IN_STACK_B	ps->sz_a - 3
+# define PS_PREVIOUS_MAX			PS_MAX_INDEX_IN_STACK_B - 1
+# define PS_MEDIAN					PS_MAX_INDEX_IN_STACK_B / 2
+# define PS_COUNT_MIN_AT_BOTTOM		0
+# define SIZE_A						ps->sz_a
+# define SIZE_B						ps->sz_b
+# define TOP_A						(*a)->i
+# define SECOND_A					(*a)->next->i
+# define TOP_B						(*b)->i
+# define SECOND_B					(*b)->next->i
+# define IS_OPER_OF_STACK_A			(op->next->oper[1] == 'a' || op->next->oper[2] == 'a')
+# define IS_OPER_OF_STACK_B			(op->next->oper[1] == 'b' || op->next->oper[2] == 'b')
 
 typedef struct		s_oper
 {
@@ -48,6 +49,7 @@ typedef struct		s_max_min
 	int				max;
 	int				pmax;
 	int				min;
+	int				mid;
 }					t_max_min;
 
 void				sort(t_ps *ps);
