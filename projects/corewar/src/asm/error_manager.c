ac778d62395900e8adf0d1ba3f1bc590ded725b9
ildison <ildarusmanov92@gmail.com>
2019-12-30 15:12:16 +0300

    fix func error_manager

--- projects/corewar/src/asm/error_manager.c
+++ projects/corewar/src/asm/error_manager.c
@@ -6,7 +6,7 @@
 /*   By: cormund <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/24 12:28:36 by cormund           #+#    #+#             */
-/*   Updated: 2019/12/24 18:14:08 by cormund          ###   ########.fr       */
+/*   Updated: 2019/12/30 15:07:26 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,30 @@
 void	check_ln_col(int *ln, int *col, char *data, char *ptr_error)
 {
 	*ln = 1;
-	*col = 1;
+	*col = 0;
 	while (*data)
 	{
+		*col += *data != '\t' ? 1 : 4;
+		// if (*data != '\t')
+		// 	*col += 4;
+		// ++*col;
 		if (data == ptr_error)
 			break ;
-		++*col;
 		if (*data == '\n')
 		{
 			++*ln;
-			*col = 1;
+			*col = 0;
 		}
 		++data;
 	}
 }
 
-void	error_manager(char *error, char *data, char *ptr_error)
+void	error_manager(char *error)
 {
 	int	ln;
 	int	col;
 
-	check_ln_col(&ln, &col, data, ptr_error);
+	check_ln_col(&ln, &col, ASM_INPUT, ASM_DATA);
 	ft_printf("%s [%d:%d]\n", error, ln, col);
 	exit(-1);
 }
