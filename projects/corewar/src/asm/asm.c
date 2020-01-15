92caa8d6204deedd54b6a644f20c3a2434e2db45
ildison <ildarusmanov92@gmail.com>
2020-01-15 10:45:51 +0300

    add func check_filename

--- projects/corewar/src/asm/asm.c
+++ projects/corewar/src/asm/asm.c
@@ -6,7 +6,7 @@
 /*   By: cormund <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/23 16:20:12 by cormund           #+#    #+#             */
-/*   Updated: 2020/01/15 09:03:32 by cormund          ###   ########.fr       */
+/*   Updated: 2020/01/15 10:31:31 by cormund          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,15 @@ static char	*new_filename(char *file)
 	return (new);
 }
 
+static void	check_filename(char *file)
+{
+	int		len;
+
+	len = ft_strlen(file);
+	if (!len || file[len - 2] != '.' || file[len - 1] != 's')
+		error("Wrong filename (need .s)");
+}
+
 int			main(int argc, char **argv)
 {
 	t_champ	*champ;
@@ -35,12 +44,14 @@ int			main(int argc, char **argv)
 		champ = ft_memalloc(sizeof(t_champ));
 		if (!champ)
 			error(strerror(errno));
+		check_filename(argv[1]);
 		champ->file_name = new_filename(argv[1]);
 		ASM_INPUT = read_data(argv[1]);
 		ASM_EOL = NULL;
 		clean_comments(ASM_INPUT);
 		parsing_champ(champ);
 		translate_in_byte_code(champ);
+		ft_printf("Writing output program to %s\n", champ->file_name);
 	}
 	return (0);
 }
